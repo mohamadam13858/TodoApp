84b571845235807aa994af14d5e980cf360a6095
mohamad <mh711748@gmail.com>
2025-04-19 02:32:40 +0330

    service

--- TodoApp/Todo-App/src/app/app.module.ts
+++ TodoApp/Todo-App/src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/layout/header/header.component';
 import { FooterComponent } from './components/layout/footer/footer.component';
 import { TodoComponent } from './components/todo/todo.component';
 import { CategoryComponent } from './components/category/category.component';
-
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore'
+import { AngularFireModule } from '@angular/fire/compat'
+import { environment } from '../environment';
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,7 +19,9 @@ import { CategoryComponent } from './components/category/category.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ TodoApp/Todo-App/src/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    firebaseConfig: {
+        apiKey: "AIzaSyCvnvAMVDpy9nUXCn4qriSBLCknolDu4o0",
+        authDomain: "todo-pro-629b9.firebaseapp.com",
+        projectId: "todo-pro-629b9",
+        storageBucket: "todo-pro-629b9.firebasestorage.app",
+        messagingSenderId: "353099499724",
+        appId: "1:353099499724:web:f274ee4da144ab5def9a69"
+    }
+}
